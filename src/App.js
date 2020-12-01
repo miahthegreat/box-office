372d086227a94e87e887dfe48810672ce932f2de
miahthegreat <jjschmid85@gmail.com>
2020-11-30 23:01:30 -0500

    styled show page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,6 @@ import Home from './pages/Home';
 import Show from './pages/Show';
 import Starred from './pages/Starred';
 
-const theme = {
-  mainColors: {
-    blue: '#2400ff',
-    gray: '#c6c6c6',
-    dark: '#353535',
-  },
-};
-
 function App() {
   return (
     <div>
