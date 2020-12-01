4342e5d75c00cce0b984954e3e2efb2d29edaa67
miahthegreat <jjschmid85@gmail.com>
2020-11-30 21:29:43 -0500

    created show page components and populated them with data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Home from './pages/Home';
 import Show from './pages/Show';
 import Starred from './pages/Starred';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
 function App() {
   return (
     <div>
