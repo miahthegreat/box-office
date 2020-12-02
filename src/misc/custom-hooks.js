ffe33b1c98d2f02836c649a47f33da0825c28ca1
miahthegreat <jjschmid85@gmail.com>
2020-12-01 23:57:52 -0500

    defined theme for styled components and created a few style components

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,5 @@
-import { useReducer, useEffect} from 'react';
+import { useReducer, useEffect, useState } from 'react';
+import { apiGet } from './config';
 
 function showsReducer(prevState, action){
   switch(action.type){
@@ -16,7 +17,7 @@ function showsReducer(prevState, action){
 }
 
 function usePersistedReducer(reducer, initialState, key) {
-  const [state, dispatch] = useReducer(reducer, initialState, (initial) => {
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
     const persisted = localStorage.getItem(key);
 
     return persisted ? JSON.parse(persisted) : initial;
@@ -31,4 +32,67 @@ function usePersistedReducer(reducer, initialState, key) {
 
 export function useShows(key = 'shows'){
   return usePersistedReducer(showsReducer, [], key);
+}
+
+export function useLastQuery(key = 'lastQuery') {
+  const [input, setInput] = useState( () => {
+    const persisted = sessionStorage.getItem(key);
+
+    return persisted ? JSON.parse(persisted) : '';
+  });
+
+  const setPersistedInput = newState => {
+    setInput(newState);
+    sessionStorage.setItem(key, JSON.stringify(newState));
+  }
+  return [input, setPersistedInput]
+}
+
+const reducer = (prevState, action) => {
+  switch(action.type){
+    
+    case 'FETCH_SUCCESS': {
+      return {isLoading: false, show: action.show, error: null}
+    }
+
+    case 'FETCH_FAILED': {
+      return {...prevState, isLoading: false, error: action.error}
+    }
+
+    default: return prevState
+  }
+}
+
+export function useShow(showId) {
+  const [state, dispatch] = useReducer(
+    reducer,
+    {
+      show: null,
+      isLoading: true,
+      error: null
+    });
+
+  useEffect(()=> {
+
+    let isMounted = true;
+
+    apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`)
+    .then(results => {
+      setTimeout(()=>{
+        if(isMounted){
+          dispatch({ type: 'FETCH_SUCCESS', show: results});
+        }
+      }, 2000);
+    }).catch(err => {
+      if(isMounted){
+        dispatch({type: 'FETCH_FAILED', error: err.message})
+      }
+    })
+
+    return () => {
+      isMounted = false;
+    }
+  }, [showId]);
+
+  return state;
 }
